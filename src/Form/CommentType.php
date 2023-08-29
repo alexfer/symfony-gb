ba26994216633ed59292247bdb7ab76c021ab6dc
alexander <AlexanderSh>
2023-08-29 13:20:29 +0300

    added missing files

---
+++ src/Form/CommentType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+
+class CommentType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('author', TextType::class)
+                ->add('email', EmailType::class, [])
+                ->add('content', CKEditorType::class)
+                ->add('captcha', CaptchaType::class, [
+                    'invalid_message' => 'Bad Captcha code',
+                    'label_attr' => [
+                        'class' => 'mt-4 mb-4',
+                        'for' => "captcha",
+        ]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
