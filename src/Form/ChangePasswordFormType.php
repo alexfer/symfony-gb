d90e4c1f7f0d472ebfe30142670836685284e475
alexander <AlexanderSh>
2023-09-05 16:44:53 +0300

    reset password implementation

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'options' => [
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                ],
+            ],
+            'first_options' => [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                            ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                            ]),
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'New password',
+            ],
+            'second_options' => [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Repeat Password',
+                'label_attr' => [
+                    'class' => 'mt-3',
+                ],
+            ],
+            'invalid_message' => 'The password fields must match.',
+            // Instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            'mapped' => false,
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
